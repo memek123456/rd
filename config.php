1c7840828fd40117fbf951b8f6b22d98765a234b
memek123456 <46232817+memek123456@users.noreply.github.com>
2019-03-02 16:07:57 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+///jika login ke bot jangan login ke apk biar gak abnormal akun nya
+///No banned No asik
+
+$username = 'ilhamsah6662';
+$pass = 'bfejovao';
+
+///isi dengan model brand kalian Ex. SM-N930V
+$model = "SM-N930V";
+
+///isi dengan brand yang kalian Ex. Samsung
+$brand = "Samsung";
